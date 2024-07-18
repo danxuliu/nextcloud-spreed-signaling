3f4de60657770bf4658feba61605319759f6e4aa
Joachim Bauch <bauch@struktur.de>
2024-07-18 14:49:04 +0200

    Expect base signaling url for federation requests.

--- federation.go
+++ federation.go
@@ -93,7 +93,7 @@ func NewFederationClient(ctx context.Context, hub *Hub, session *ClientSession, 
 	case "https":
 		u.Scheme = "wss"
 	}
-	conn, response, err := dialer.DialContext(ctx, u.String(), nil)
+	conn, response, err := dialer.DialContext(ctx, u.String()+"spreed", nil)
 	if err != nil {
 		return nil, err
 	}
